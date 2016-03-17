04cd3ae1294214d95bd450b0806ba249332cfa3c
Paulo Henrique <paulohenriquesi@live.com>
2016-03-17 16:41:33 -0300

    Versão inicial

---
+++ TestesEficientesParte1Demo.Tests/DemoServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+
+namespace TestesEficientesParte1Demo.Tests
+{
+    public class DemoServiceTests
+    {
+        [Theory]
+        [InlineData(7, true)]
+        [InlineData(8, false)]
+        [InlineData(23, true)]
+        [InlineData(179, true)]
+        [InlineData(463, true)]
+        [InlineData(617, true)]
+        [InlineData(649, false)]
+        [InlineData(811, true)]
+        [InlineData(907, true)]
+        [InlineData(972, false)]
+        [InlineData(997, true)]
+        public void IsPrimeNumber_WhenNumberIs7_ShouldReturnTrue(int number, bool expectedResult)
+        {
+            var service = new Demo1Service();
+
+            Assert.Equal(service.IsPrimeNumber(number), expectedResult);
+        }
+    }
+}
---
+++ TestesEficientesParte1Demo/Demo1Service.cs
@@ -0,0 +1,19 @@
+﻿namespace TestesEficientesParte1Demo
+{
+    public class Demo1Service
+    {
+        public bool IsPrimeNumber(int number)
+        {
+            for (int i = 2; i < number; i++)
+            {
+                int resto = number % i;
+                if (resto == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
